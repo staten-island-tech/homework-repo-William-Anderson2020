215b4fc4aa72a174b4e48626b56e9f9dd2c2d6b4
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-09-18 14:57:49 -0400

    hw reformat and beginings of hw4

--- HW1.js
+++ HW1.js
@@ -1,4 +1,4 @@
-/* /* /* /* /* Hw#1 */
+/* Hw#1 */
 let x = 2 + "2"; /// console.log(x) = 22. Displays 2, then adds string of 2
 let y = 2 + 2 + "2"; /// console.log(y) = 42. The first two are numbers which are added, then string of 2 is added to end
 let z = 2 + true; /// console.log(z) = 3. True = 1, False = 0
@@ -7,6 +7,7 @@ let xy = true + "2"; /// console.log(xy) = true2 (Boolean to String is easier th
 let xz = Number("Hello"); /// console.log(xz) = NaN (Not a number) String cannot be converted to number
 let yx = Boolean(5); /// Boolean conversion is always true unless 0
 let yy = Boolean("Hello"); /// String to Boolean is always true unless string is empty (Null and undefined are also false)
+let yz = '';
 if(yz){} /// Will check if yz is an empty string, false, 0, undefined, or null. Only works for primitive tags
 
 /* Hw#2 */
@@ -14,7 +15,7 @@ function greet(firstName = 'John', lastName = 'Doe'){ //Lists defaults if a valu
     return `Hello ${firstName + lastName}.`;
 }
 
-console.log(greet(Simon)); // Returns Simon Doe. Input first name, default last.
+console.log(greet('Simon')); // Returns Simon Doe. Input first name, default last.
 
 let square = function(x){ //annonymous functions take on the name of the variable. ie, square();
     return x*x;
@@ -44,11 +45,21 @@ todo.delete = function(){ //Can be added outside of variable as well.
 todo.add();
 todo.edit(22);
 
+
+
 /*HW #4 Scopes*/
 
-let x = 3;
+let q = 3;
 {
-    x=5;
+    q=5; //in the same scope as q=3
 }
 
-console.log(x);
+console.log(q);
+
+function normandy(){
+    let beach = 'gold';
+    console.log(`Welcome to ${beach} landing zone.`)
+}
+normandy();
+
+//console.log(beach); //won't work since beach is described in the function scope, not global.
