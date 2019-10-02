332097e9bdb5dbdfd5ebe862bc2238e56a7d0715
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-10-01 20:04:32 -0400

    hw 5 start

---
+++ hw5.js
@@ -0,0 +1,29 @@
+// Q1.READ ALL THE INSTRUCTIONS CAREFULLY
+  // Select all the list items on the page and convert to array (array from)
+
+    const itemsList = document.querySelectorAll('li');
+    const itemsListArray = Array.prototype.slice.call(itemsList);
+    //console.log(itemsListArray);
+    
+  // Filter for only the elements that contain the word 'Flexbox' hint use textContent (filter method)
+
+    function itemsListFlexboxCheck(item){
+        if( item.textContent == 'Flexbox'){
+            return item;
+        }
+    }
+    const itemsListFlexbox = itemsListArray.filter(itemsListFlexboxCheck); //?
+    console.log(itemsListFlexbox);
+
+  // map down to a list of time strings Hint look up dataset mdn and think 'time'... you will need to create a new variable called filtered 
+  
+  
+
+
+  // map to an array of seconds, 
+  /* .map(timeCode => {
+    const parts = timeCode.split(':').ENTER SOME CODE HERE);
+    //console.log(parts); 
+    return SOME CODE GOES HERE
+}) */
+
